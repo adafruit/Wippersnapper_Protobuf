667d26ba9847a1f3240d789b1072d5302890f842
brentru <robots199@me.com>
2020-06-18 16:59:32 -0400

    add pin # to command message, not sure if we should break this into differnet command types instead of just a centralized Command

--- proto/signal/v1/test_signal_proto.py
+++ proto/signal/v1/test_signal_proto.py
@@ -4,7 +4,7 @@ signal = signal_pb2.Signal()
 
 # add new command to signal
 cmd = signal.commands
-# pack command 
+# add command information
 cmd.type = signal.COMMAND_TYPE_VERSION
 cmd.direction = signal.COMMAND_DIRECTION_SET
 cmd.value= "0.0.1"
