9befebe88684f1e37727a02b3fcf79bb9836150a
brentru <robots199@me.com>
2020-06-22 13:45:55 -0400

    add working blinkaconnect semver example

--- examples_python/get_blinkaconnect_version.py
+++ examples_python/get_blinkaconnect_version.py
@@ -1,6 +1,11 @@
 # SPDX-FileCopyrightText: 2020 Brent Rubell for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
+# strict system paths
+import sys
+sys.path.insert(1, 'semver/v1')
+sys.path.insert(1, 'signal/v1')
+
 import signal_pb2
 
 def serialize_protobuf(buf):
@@ -17,32 +22,27 @@ def serialize_protobuf(buf):
 signal = signal_pb2.Signal()
 
 """
-PUBLISH from Adafruit IO to Device
-
-Request library version from device
+GET library version from device.
 """
 command = signal.cmd
 # command message
 command.type = signal.CMD_TYPE_GET
 command.name = signal.CMD_NAME_VERSION
 
-print(signal)
-serialize_protobuf(signal)
+print(command)
 signal.Clear()
 
-"""
-PUBLISH from Device to Adafruit IO
 
-Send library version from device to Adafruit IO
+"""
+GET library version from device.
 """
 command = signal.cmd
 # command message
 command.type = signal.CMD_TYPE_SET
 command.name = signal.CMD_NAME_VERSION
+# version message
+command.version.major = 1
+command.version.minor = 2
+command.version.micro = 3
 
-# TODO: This is lacking a way to
-# hold the semantic version within signal!
-
-print(signal)
-serialize_protobuf(signal)
-signal.Clear()
+print(command)
