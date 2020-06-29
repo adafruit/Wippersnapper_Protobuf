e9a191d274529aa782cb0ceeea0f0e65031e0eb9
brentru <robots199@me.com>
2020-06-29 17:01:36 -0400

    add example for sending data to io (sensor_event)

--- examples_python/temperature_sensor.py
+++ examples_python/temperature_sensor.py
@@ -19,23 +19,36 @@ cmd.type = signal.CMD_TYPE_SENSOR
 temperature = cmd.sensors.add()
 temperature.type.name = "ADT7410"
 temperature.type.sensor_id = 0x01
-temperature.type.type = temperature.type.SensorType.            SENSOR_TYPE_AMBIENT_TEMPERATURE = 13;
+temperature.type.type = temperature.type.SensorType.SENSOR_TYPE_AMBIENT_TEMPERATURE = 13;
 temperature.type.measurement_period = 0
-
-# NOTE: problem, min of adt7410 is -55C, incomat. type with min value field
-temperature.type.min_value = 0
-
+temperature.type.min_value = -55.0
 temperature.type.max_value = 150.0
 temperature.type.resolution = 0.01
 
 print(cmd)
 
+signal.Clear()
+
 
 """
-TODO: Request sensor info from Adafruit IO
+Send data to Adafruit IO
 """
+cmd = signal.command
+cmd.mode = signal.CMD_MODE_GET
+cmd.type = signal.CMD_TYPE_SENSOR
+
+# add a new sensor to the command
+temperature = cmd.sensors.add()
+temperature.event.sensor_id = 0x01 # request same sensor identifier
+temperature.event.sensor_type = 13
+temperature.event.timestamp = 100
+temperature.event.temperature = 42
+
+print(cmd)
+
+signal.Clear()
 
 """
-TODO: Set up and request multiple sensor information
+TODO: Set up and request information from multiple types of temperature sensors
 NOTE: 2x examples for this
 """
