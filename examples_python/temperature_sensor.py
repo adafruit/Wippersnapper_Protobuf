33cf3ddf57aef47a8d50587d67af87bfdfdbfb94
brentru <robots199@me.com>
2020-07-03 12:02:10 -0400

    example calls cmd.sensor directly instead of creating new object

--- examples_python/temperature_sensor.py
+++ examples_python/temperature_sensor.py
@@ -4,25 +4,22 @@ import sys
 sys.path.insert(1, 'signal/v1')
 import signal_pb2
 
-# Create new message
-signal = signal_pb2.Signal()
 
-# Set up sensor
+# Send message from device advertising a temperature sensor to Adafruit IO
+# NOTE: This will create a new `component` on the device's definition
+signal = signal_pb2.Signal()
 cmd = signal.command
 cmd.mode = signal.CMD_MODE_SET
 cmd.type = signal.CMD_TYPE_SENSOR
-
-# add a new sensor to the command
-temperature = cmd.sensor
-temperature.type.name = "adt" # sensor's name, as defined in user-code
-temperature.type.sensor_id = 0x01 # sensor identifier, automatically incremented in BlinkaConnect library
-sensor_type_temp = temperature.SensorType.SENSOR_TYPE_AMBIENT_TEMPERATURE
-temperature.type.type = sensor_type_temp # sensor type
-temperature.type.measurement_period = 0 # read sensor as quickly as possible
-temperature.type.min_value = -55.0 # min value, -55.0C
-temperature.type.max_value = 150.0 # max value, 150.0C
-temperature.type.resolution = 0.01 # sensor resolution
-
+# Configure required sensor properties
+cmd.sensor.type.name = "adt"
+cmd.sensor.type.sensor_id = 0x01
+# Configure optional sensor properties
+# TODO: change this to sensor_type
+cmd.sensor.type.type = cmd.sensor.SensorType.SENSOR_TYPE_AMBIENT_TEMPERATURE
+cmd.sensor.type.measurement_period = 0
+cmd.sensor.type.min_value = -55.0
+cmd.sensor.type.max_value = 150.0
+cmd.sensor.type.resolution = 0.01
 print(cmd)
-
 signal.Clear()
